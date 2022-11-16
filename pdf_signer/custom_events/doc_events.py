0d34f5ab4f574602c01dd5b7feb64d34e2114a51
Danny Molina Morales <mmdanny89@gmail.com>
2022-11-16 08:30:16 -0500

    fix: remove unused bundle for app

--- pdf_signer/custom_events/doc_events.py
+++ pdf_signer/custom_events/doc_events.py
@@ -7,19 +7,6 @@ Version :   1.0
 E-mail  :   mmdanny89@gmail.com
 """
 
-import frappe
-
 
 def remove_file_signed(doc, method):
     print("hello world")
-
-
-def check_file_uploaded(doc, method):
-    ask_way = frappe.db.get_single_value("Global Settings PDF Signer", "always_ask")
-    if ask_way == 1:
-        signature_setting = frappe.db.get("Electronic Sign Setting", filters={"status": "Enable", "is_default": 1})
-        if signature_setting:
-            frappe.publish_realtime(event="ask_to_sign", message={"task_id": 10000001, "foo": "bar"}, user=frappe.session.user)
-        else:
-            signs_enable = frappe.db.get_list("Electronic Sign Setting", filters={"status": "Enable"}, pluck="name")
-            frappe.publish_realtime("ask_to_sign", message={"name_sign": signs_enable, "file_name": doc.name}, user=frappe.session.user)
--- pdf_signer/hooks.py
+++ pdf_signer/hooks.py
@@ -13,7 +13,6 @@ app_version = app_version
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/pdf_signer/css/pdf_signer.css"
-app_include_js = ["pdf_signer.bundle.js"]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/pdf_signer/css/pdf_signer.css"
@@ -105,7 +104,6 @@ doctype_list_js = {"File": "public/js/file_list.js"}
 
 doc_events = {
     "File": {
-        "after_insert": "pdf_signer.custom_events.doc_events.check_file_uploaded",
         # "on_update": "pdf_signer.custom_events.doc_events.check_file_uploaded",
         "on_trash": "pdf_signer.custom_events.doc_events.remove_file_signed",
     }
