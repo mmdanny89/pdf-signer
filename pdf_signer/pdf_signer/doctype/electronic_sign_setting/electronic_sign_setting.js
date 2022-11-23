c704c3303643f7bde6ebae26fceb267a847938b9
Danny Molina Morales <mmdanny89@gmail.com>
2022-11-23 11:09:04 -0500

    fix: fixed customization for dc es

--- pdf_signer/pdf_signer/doctype/electronic_sign_setting/electronic_sign_setting.js
+++ pdf_signer/pdf_signer/doctype/electronic_sign_setting/electronic_sign_setting.js
@@ -1,19 +1,35 @@
 // Copyright (c) 2022, Danny Molina Morales and contributors
 // For license information, please see license.txt
 
+
 let customize_fileds = function(frm) {
-	frappe.after_ajax(function () {
-		frm.$wrapper.find('div[data-fieldname="postition"]').find('.ace-editor-target.border.rounded.ace_editor.ace_hidpi.ace-tomorrow').attr('style', 'height: 70px');
-		frm.$wrapper.find('div[data-fieldname="postition"]').find('button').hide();
+	let class_ = "ace-editor-target border rounded ace_editor ace_hidpi ace-tomorrow";
+	let mutationObserver = new MutationObserver(function(mutations) {
+		mutations.forEach(function(mutation) {
+		  if (mutation.type === "attributes" && mutation.attributeName === "class") {
+			if (mutation.target.classList.toString() === class_){
+				if (String(mutation.target.style.height) === "300px") {
+					mutation.target.style.height = "70px";
+				}
+			}
+		  }	
+		});
+	  });
+	mutationObserver.observe(document.body, {
+		attributes: true,
+		childList: true,
+		subtree: true,
 	});
-		
+	frm.$wrapper.find('div[data-fieldname="postition"]').find('button').hide();
 }
+
 
 frappe.ui.form.on('Electronic Sign Setting', {
 	onload_post_render: function(frm){
 		customize_fileds(frm);
 	},
 	refresh: function(frm) {
+		customize_fileds(frm);
 		if (!frm.doc.__islocal) {
 			if (frm.doc.status === 'Enable') {
 				frm.add_custom_button(__('Disabled Electronic Sign'), function(){
@@ -41,5 +57,16 @@ frappe.ui.form.on('Electronic Sign Setting', {
 				}).removeClass('btn-default').addClass("btn-primary");
 			}
 		}
-	}
+
+	},
+	use_bg_image: function(frm) {
+		if (frm.doc.use_bg_image) {
+			frm.set_value('use_own_sign_bg_image', 0);
+		}
+	},
+	use_own_sign_bg_image: function(frm) {
+		if (frm.doc.use_own_sign_bg_image) {
+			frm.set_value('use_bg_image', 0);
+		}
+	},
 });
